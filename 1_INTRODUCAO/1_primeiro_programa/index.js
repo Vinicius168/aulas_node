b8385a63afee2f6605c32fbddb9282ed5f078435
Vinicius Alves <viniciusgames168@gmail.com>
2023-08-29 14:22:55 -0300

    criaçao da pasta nodemon

---
+++ 1_INTRODUCAO/1_primeiro_programa/index.js
@@ -0,0 +1,1 @@
+console.log("BOM DIA MELHOR DO MUNDO!")
---
+++ 1_INTRODUCAO/3_tarefa/index.js
@@ -0,0 +1,6 @@
+//crie duas variaveis de numero e mostre no terminal a somaentre elas
+
+const NUMERALHEHE1 = 10
+const NUMERALHEHE2 = 5
+
+console.log(NUMERALHEHE1+NUMERALHEHE2)
